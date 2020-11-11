0dcd85159d0545222e8781bbf01a43f04caf6271
hogashi <hogashi@hatena.ne.jp>
2020-11-11 21:57:15 +0900

    slash only hostname exists

--- src/makeHttpsUrl.ts
+++ src/makeHttpsUrl.ts
@@ -1,5 +1,12 @@
-export const makeHttpsUrl = (inputUrl: string) =>
-  inputUrl.replace(
-    /^(?:[^:]+:\/\/|)(?:git@|)([^:\/]+)[:\/](.+?)(?:\.git|)$/,
-    'https://$1/$2',
+export const makeHttpsUrl = (inputUrl: string) => {
+  const result = inputUrl.match(
+    //   scheme         username          password    hostname   port                path       suffix
+    /^(?:[^:]+:\/\/)?(?:[a-zA-Z0-9_\-]+(?::[^@]+)?@)?([^:/]+)?(?::(?:[0-9]+)?\/?|\/)([^/].*?)(?:\.git)?$/
   );
+  if (result === null) {
+    return inputUrl;
+  }
+  const hostname = result[1] === undefined ? '' : `${result[1]}/`;
+  const path = result[2];
+  return `https://${hostname}${path}`;
+};
